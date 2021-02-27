eec64a56707c01f6a567e5a5950354491669abb5
Lars Falk-Petersen <dev@falkp.no>
2021-02-27 23:20:43 +0100

    Server running, args for port, verbose, help. Serving echo test service.

--- server.py
+++ server.py
@@ -8,23 +8,23 @@ import asyncio
 version = 1
 port=79
 
-async def handle_echo(reader, writer):
+async def handle_request(reader, writer):
     data = await reader.read(100)
     message = data.decode()
     addr = writer.get_extra_info('peername')
 
-    logging.info(f"Received {message!r} from {addr!r}")
+    logging.info(f"{addr!r} Received: {message!r}")
 
-    logging.info(f"Send: {message!r}")
+    logging.info(f"{addr!r} Sent: {message!r}")
     writer.write(data)
     await writer.drain()
 
-    logging.info("Closing the connection")
+    logging.info("{addr!r} Closing connection")
     writer.close()
 
 async def main():
     server = await asyncio.start_server(
-        handle_echo, '0.0.0.0', port)
+        handle_request, '0.0.0.0', port)
 
     addr = server.sockets[0].getsockname()
     logging.info(f'Serving on {addr}')
@@ -32,22 +32,26 @@ async def main():
     async with server:
         await server.serve_forever()
 
+def help():
+    print ("Arguments:\n-h\tHelp\n-p\tPort number (default 79, needs root)")
+    sys.exit()
+
 
 if __name__ == "__main__":
 
     try:
-        opts, args = getopt.getopt(sys.argv,"hp:")
+        options, remainder = getopt.getopt(sys.argv[1:],"hvp:")
     except getopt.GetoptError:
-        print (f'{sys.argv[0]} -p <port number>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print (f'{sys.argv[0]} -h')
-            sys.exit()
+        print("Error, check arguments.")
+        help()
+
+    for opt, arg in options:
+        if opt in ['-h', '--help']:
+            help()
+        if opt in ['-v', '--verbose']:
+            logging.basicConfig(level=logging.INFO)
         if opt == '-p':
             port = arg
             logging.info(f'Port set to {port}')
-
-    logging.basicConfig(level=logging.INFO)
 
     asyncio.run(main())
