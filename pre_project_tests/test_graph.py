1643b7335291623b5a6c3ad46aa067379a0ee985
Lars Falk-Petersen <larsfp@met.no>
2025-11-02 11:18:09 +0100

    Remove stupid AI generated test files

--- pre_project_tests/test_graph.py
+++
@@ -1,9 +0,0 @@
-from metno_locationforecast import Place, Forecast
-new_york = Place("Oslo/Norway", 59, 10)
-ny_forecast = Forecast(new_york, "fingr/1.0 https://graph.no")
-ny_forecast.update()
-
-import server
-
-response = server.format_meteogram(ny_forecast)
-print (response)
--- pre_project_tests/test_location.py
+++
@@ -1,7 +0,0 @@
-from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="graph.no")
-location = geolocator.geocode("Oslo/Norway")
-
-print(location.address)
-print((location.latitude, location.longitude))
-print(location.raw)
